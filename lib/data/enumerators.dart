6121644dc2e39aa78d7925d071a782bee77a5e80
Luc1eSky <schulz.lucie@gmail.com>
2024-08-15 17:17:50 -0700

    updated enumerators

--- lib/data/enumerators.dart
+++ lib/data/enumerators.dart
@@ -3,12 +3,12 @@
 import '../classes/enumerator.dart';
 
 List<Enumerator> enumerators = const [
-  Enumerator(firstName: "Dexter", lastName: "S."),
-  Enumerator(firstName: "Ellimo", lastName: "M."),
-  Enumerator(firstName: "Emelia", lastName: "B."),
-  Enumerator(firstName: "Hendrix", lastName: "K."),
-  Enumerator(firstName: "Joseph", lastName: "M."),
-  Enumerator(firstName: "Sikananu", lastName: "S."),
-  Enumerator(firstName: "Sylvia", lastName: "S."),
-  Enumerator(firstName: "Tapiwa", lastName: "J."),
+  Enumerator(firstName: "Kaumba", lastName: "C."),
+  Enumerator(firstName: "Franklin", lastName: "M."),
+  Enumerator(firstName: "Steven", lastName: "M."),
+  Enumerator(firstName: "Deborah", lastName: "M."),
+  Enumerator(firstName: "Matilda", lastName: "S."),
+  Enumerator(firstName: "Precious", lastName: "N."),
+  Enumerator(firstName: "Inutu", lastName: "M."),
+  Enumerator(firstName: "Never", lastName: "P."),
 ];
